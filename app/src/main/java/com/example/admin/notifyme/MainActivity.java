82a66dad7eb2848a192ab818487088d2ebbb2711
mahi-02 <mahimabansal24@gmail.com>
2018-05-01 06:33:50 -0700

    changes in main activity

--- app/src/main/java/com/example/admin/notifyme/MainActivity.java
+++ app/src/main/java/com/example/admin/notifyme/MainActivity.java
@@ -4,13 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Intent myintent;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent i = new Intent(this,LoginActivity.class);
-        startActivity(i);
+        setContentView(R.layout.splash);
+
+        myintent = new Intent(this, MainActivity.class);
+        splashScreen(3000); }
+
+    public void splashScreen (final int x)
+    {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(myintent);
+                finish();
+            }
+        }).start();
     }
+
 }
